b8cae3dee5ee916fa5e190c7a8653ce7fce4afcb
Benedikt Kulmann <benedikt@kulmann.biz>
2019-10-14 14:46:48 +0200

    Added file serving to the plugin.

--- lib.php
+++ lib.php
@@ -31,6 +31,7 @@
 
 use mod_philosophers\form\form_controller;
 use mod_philosophers\model\gamesession;
+use mod_philosophers\model\level;
 use mod_philosophers\util;
 
 defined('MOODLE_INTERNAL') || die();
@@ -224,4 +225,49 @@ function philosophers_perform_completion($course, $cm, $philosophers) {
     if ($completion->is_enabled($cm) == COMPLETION_TRACKING_AUTOMATIC && ($philosophers->completionrounds || $philosophers->completionpoints)) {
         $completion->update_state($cm, COMPLETION_COMPLETE, $USER->id);
     }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// File API                                                                   //
+////////////////////////////////////////////////////////////////////////////////
+
+function philosophers_pluginfile($course, $cm, $context, $filearea, $args, $forcedownload, array $options = []) {
+    // Check the contextlevel is as expected - if your plugin is a block, this becomes CONTEXT_BLOCK, etc.
+    if ($context->contextlevel != CONTEXT_MODULE) {
+        return false;
+    }
+
+    // Make sure the filearea is one of those used by the plugin.
+    if ($filearea !== level::FILE_AREA) {
+        return false;
+    }
+
+    // Make sure the user is logged in and has access to the module (plugins that are not course modules should leave out the 'cm' part).
+    require_login($course, true, $cm);
+
+    // Check the relevant capabilities - these may vary depending on the filearea being accessed.
+    if (!has_capability('mod/philosophers:view', $context)) {
+        return false;
+    }
+
+    // Get the item id
+    $itemid = array_shift($args); // The first item in the $args array.
+
+    // Extract the filename / filepath from the $args array.
+    $filename = array_pop($args); // The last item in the $args array.
+    if (!$args) {
+        $filepath = '/'; // $args is empty => the path is '/'
+    } else {
+        $filepath = '/'.implode('/', $args).'/'; // $args contains elements of the filepath
+    }
+
+    // Retrieve the file from the Files API.
+    $fs = get_file_storage();
+    $file = $fs->get_file($context->id, 'mod_philosophers', $filearea, $itemid, $filepath, $filename);
+    if (!$file) {
+        return false; // The file does not exist.
+    }
+
+    // We can now send the file back to the browser - in this case with a cache lifetime of 1 day and no filtering.
+    send_stored_file($file, 86400, 0, $forcedownload, $options);
 }
