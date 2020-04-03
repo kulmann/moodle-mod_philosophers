7b2985184a9211ae0d638fc306f1d4cb55bc7419
Benedikt Kulmann <benedikt@kulmann.biz>
2020-04-03 19:12:54 +0200

    Version bump for release 0.3.17

--- version.php
+++ version.php
@@ -30,5 +30,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'mod_philosophers';
 $plugin->requires = 2017111302;
 $plugin->maturity = MATURITY_BETA;
-$plugin->version = 2020040300;
+$plugin->version = 2020040301;
 $plugin->release = '0.3.17';
